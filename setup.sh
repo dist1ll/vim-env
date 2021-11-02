413c61936f8deea2b8054286f06573a826923ca7
Adrian <alic.adrian.berlin@gmail.com>
2021-11-02 15:54:54 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-
 mkdir -p ~/.vim/pack
-(cd ~/ && curl -O https://raw.githubusercontent.com/m2q/vim-env/main/_vimrc)
-mv _vimrc .vimrc
+curl https://raw.githubusercontent.com/m2q/vim-env/main/_vimrc > ~/.vimrc
 (cd ~/.vim/pack/ && git clone https://github.com/m2q/vim-env .)
